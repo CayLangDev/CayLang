376acb3c4abfacf47bc73f6d6e7d1aecf4d444b8
Lachlan Westfall <lochiewestfall@gmail.com>
2023-09-09 17:12:57 +1000

    Tweak flatten example

--- caylang/dir-operations/src/main.rs
+++ caylang/dir-operations/src/main.rs
@@ -2,51 +2,40 @@ use std::env;
 pub mod tree;
 use std::path::PathBuf;
 pub mod filesys;
-use crate::tree::{Tree, NodeIdx, root_idx, NodeType};
 use crate::filesys::{load_full_tree, write_full_tree};
+use crate::tree::{root_idx, NodeIdx, NodeType, Tree};
 mod treeTests;
 
 fn dfs(tree: &Tree, current_idx: NodeIdx) {
-	let root = &tree.nodes[current_idx];
-	println!("{:?}", root.data.path);
-	for child in tree.get_children(current_idx) {
-		dfs(&tree, child);
-	}
+    let root = &tree.nodes[current_idx];
+    println!("{:?}", root.data.path);
+    for child in tree.get_children(current_idx) {
+        dfs(&tree, child);
+    }
 }
 
-// fn bfs(tree: &Tree, current_idx: NodeIdx) {
-// 	// ...
-// 	// tbd
-// }
-
 fn main() -> std::io::Result<()> {
-	let args: Vec<String> = env::args().collect();
-	if args.len() != 3 {
-		println!("Err: Expected two path arguments");
-		return Ok(());
-	}
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Flatten operation failed!\nExpected:\ncargo run <path>");
+        return Ok(());
+    }
 
-	let from_path = PathBuf::from(&args[1]);
-	let to_path = PathBuf::from(&args[2]);
+    let tree: Tree = load_full_tree((&args[1]).into());
 
-	let tree: Tree = load_full_tree((&args[1]).into());
+    let flattened_tree = Tree::from_fold_function(&tree, |x| {
+        let string = x.data.path.to_str().unwrap();
+        PathBuf::from(str::replace(string, "/", "_"))
+    });
 
-	let flattened_tree = Tree::from_fold_function(&tree, |x| {
-		let string = x.data.path.to_str().unwrap();
-		PathBuf::from(str::replace(string, "/", "_"))
-	});
+    println!("DFS");
 
-	// // println!("BFS");
-	// // bfs(&tree, root_idx());
+    dfs(&tree, root_idx());
 
-	println!("DFS");
+    println!("DFS Flatten Fold");
+    dfs(&flattened_tree, root_idx());
 
-	dfs(&tree, root_idx());
+    write_full_tree(&tree, &flattened_tree);
 
-	println!("DFS Flatten Fold");
-	dfs(&flattened_tree, root_idx());
-
-	write_full_tree(&tree, &flattened_tree);
-	
-	return Ok(());
+    return Ok(());
 }
