d54d8414d7270b9463d7e9813c1a9a81e7d699ac
Lachlan Westfall <lochiewestfall@gmail.com>
2023-10-21 13:23:43 +1100

    Add filesys helper

--- caylang/dir-operations/src/main.rs
+++ caylang/dir-operations/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 pub mod tree;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Child;
 pub mod filesys;
 use crate::filesys::{load_full_tree, write_full_tree};
 use crate::tree::{root_idx, NodeIdx, Tree};
-mod tree_tests;
+mod filesys_builder;
 mod filesys_tests;
+mod tree_tests;
+
+use crate::filesys_builder::FileNode;
 
 fn dfs(tree: &Tree, current_idx: NodeIdx) {
     let root = &tree.nodes[current_idx];
@@ -17,28 +21,30 @@ fn dfs(tree: &Tree, current_idx: NodeIdx) {
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 2 {
         println!("Flatten operation failed!\nExpected:\ncargo run <path>");
         return Ok(());
     }
 
     let path = PathBuf::from(&args[1]);
-    let to_path = PathBuf::from(&args[2]);
-    let tree: Tree = load_full_tree(&path);
+    // let to_path = PathBuf::from(&args[2]);
+    // let tree: Tree = load_full_tree(&path);
 
-    let flattened_tree = Tree::from_fold_function(&tree, |x| {
-        let string = x.data.path.to_str().unwrap();
-        PathBuf::from(str::replace(string, "/", "_"))
-    });
+    // let flattened_tree = Tree::from_fold_function(&tree, |x| {
+    //     let string = x.data.path.to_str().unwrap();
+    //     PathBuf::from(str::replace(string, "/", "_"))
+    // });
 
-    println!("DFS");
+    // println!("DFS");
 
-    dfs(&tree, root_idx());
+    // dfs(&tree, root_idx());
 
-    println!("DFS Flatten Fold");
-    dfs(&flattened_tree, root_idx());
+    // println!("DFS Flatten Fold");
+    // dfs(&flattened_tree, root_idx());
 
-    write_full_tree(&path, &to_path, &tree, &flattened_tree);
+    // write_full_tree(&path, &to_path, &tree, &flattened_tree);
+
+    // FileNode::dir("a", vec![FileNode::file("b")]).apply(&path)?;
 
     return Ok(());
 }
