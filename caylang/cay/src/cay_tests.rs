f4de2604de2546805dbca642b8bb20689ac1b1f2
Lachlan Westfall <lochiewestfall@gmail.com>
2023-10-21 13:51:23 +1100

    Remove implicit file extension

--- caylang/cay/src/cay_tests.rs
+++ caylang/cay/src/cay_tests.rs
@@ -1,11 +1,15 @@
 #[cfg(test)]
 mod tests {
     use caylang_io::filesys::run_system_test;
+    use caylang_io::filesys_builder::FileNode;
     use caylang_io::tree::{NodeData, NodeType, Tree};
+    use std::fs::File;
     use std::path::PathBuf;
 
     #[test]
     fn basic_system_shuffle() {
+        let in_structure = FileNode::dir("a", vec![FileNode::dir("b", [FileNode::file("f")])]);
+
         run_system_test("test/cases/simple_test_1");
     }
 }
