29dacc8e8d4e451324765b99472428edeee2aa3c
WSaffery <william.saffery@gmail.com>
2023-10-22 13:24:20 +1100

    restore filesys

---
+++ caylang/dir-operations/src/filesys.rs
@@ -0,0 +1,200 @@
+use crate::filesys_builder::FileNode;
+use crate::tree::{NodeData, NodeIdx, NodeType, Tree};
+use jwalk::WalkDir;
+use std::fs;
+use std::path::PathBuf;
+use std::process::{exit, Command};
+extern crate fs_extra;
+use fs_extra::dir;
+use fs_extra::dir::copy;
+
+use dir_diff::is_different;
+use tempdir::TempDir;
+
+fn dfs(tree: &Tree, current_idx: NodeIdx) {
+    let root = &tree.nodes[current_idx];
+    println!("{:?}", root.data.path);
+    for child in tree.get_children(current_idx) {
+        dfs(&tree, child);
+    }
+}
+
+pub fn system_test(
+    cay_code: &str,
+    in_structure: &FileNode,
+    out_structure: &FileNode,
+) -> Result<(), std::io::Error> {
+    let tmp_dir = TempDir::new("test").unwrap();
+    let root_path = PathBuf::from(tmp_dir.path());
+
+    let mut tmp_in = root_path.clone();
+    tmp_in.push("testbed");
+    tmp_in.push("test_root");
+
+    let mut tmp_out = root_path.clone();
+    tmp_out.push("expected");
+    tmp_out.push("test_root");
+
+    let mut tmp_cay = root_path.clone();
+    tmp_cay.push("test.cay");
+
+    fs::create_dir_all(&tmp_in)?;
+    fs::create_dir_all(&tmp_out)?;
+
+    in_structure.apply(&tmp_in)?;
+    out_structure.apply(&tmp_out)?;
+
+    fs::write(
+        &tmp_cay,
+        &cay_code.replace("<PATH>", tmp_in.to_str().unwrap()),
+    )?;
+
+    // Call main cay exec on the file
+    let mut cmd = Command::new("cargo");
+    let _ = cmd
+        .arg("run")
+        .arg("build")
+        .arg("-r")
+        .arg(tmp_cay.to_str().unwrap());
+
+    // Run cmd
+    match cmd.status() {
+        Ok(status) => status,
+        Err(e) => {
+            eprintln!("Failed to execute command: {}", e);
+            exit(1);
+        }
+    };
+
+    dfs(&load_full_tree(&root_path), 0);
+    assert!(!is_different(tmp_in.to_str().unwrap(), tmp_out.to_str().unwrap()).unwrap());
+    return Ok(());
+}
+
+pub fn run_test(path: &str, f: fn(&Tree) -> Tree) {
+    let test_path = PathBuf::from(path);
+    let mut in_path = test_path.clone();
+    in_path.push("in");
+    let mut out_path = test_path.clone();
+    out_path.push("out");
+
+    let tmp_dir = TempDir::new("test").unwrap();
+    let root_path = PathBuf::from(tmp_dir.path());
+
+    let options = dir::CopyOptions::new();
+    // options.mirror_copy = true; // To mirror copy the whole structure of the source directory
+    let _ = copy(&in_path, &root_path, &options);
+    let mut tmp_in = root_path.clone();
+    tmp_in.push("in");
+    let mut tmp_out = root_path.clone();
+    tmp_out.push("out");
+    let _ = fs::rename(&tmp_in, &tmp_out);
+
+    let tree = load_full_tree(&tmp_out);
+    let new_tree = f(&tree);
+
+    write_full_tree(&tmp_out, &tmp_out, &tree, &new_tree);
+    dfs(&tree, 0);
+    dfs(&new_tree, 0);
+
+    assert!(!is_different(out_path.to_str().unwrap(), tmp_out.to_str().unwrap()).unwrap());
+}
+
+pub fn load_full_tree(root: &PathBuf) -> Tree {
+    let mut tree: Tree = Tree::new();
+    // let mut root: NodeIdx = root_idx();
+    for entry in WalkDir::new(&root).sort(true) {
+        match entry {
+            Ok(entry) => {
+                let node_type = match entry.file_type().is_file() {
+                    true => NodeType::File,
+                    false => NodeType::Directory,
+                };
+
+                let relative_path =
+                    PathBuf::from(PathBuf::from(entry.path()).strip_prefix(&root).unwrap());
+
+                let mut parent_path = relative_path.clone();
+                parent_path.pop();
+
+                print!("{} {}\n", entry.path().display(), parent_path.display());
+                // Path sorting means we can expect the parent to exist
+                let parent_idx = tree.path_map.get(&parent_path);
+
+                println!("root: {} path: {}", root.display(), relative_path.display());
+
+                match parent_idx {
+                    Some(idx) => {
+                        tree.add_child(*idx, NodeData::new(relative_path, node_type));
+                    }
+                    None => {
+                        tree.add_child(0, NodeData::new(relative_path, node_type));
+                    }
+                }
+            }
+            _ => println!("File doesn't exist"),
+        }
+    }
+
+    return tree;
+}
+
+fn create_all_parents(path: &PathBuf) {
+    let prefix = path.as_path().parent().unwrap();
+    let _ = std::fs::create_dir_all(prefix);
+}
+
+pub fn write_full_tree(from_path: &PathBuf, to_path: &PathBuf, from_tree: &Tree, to_tree: &Tree) {
+    for node in to_tree.nodes.iter() {
+        let mut abs_path = to_path.clone();
+        abs_path.push(&node.data.path);
+        let mut abs_original_path = from_path.clone();
+        abs_original_path.push(&node.data.original_path);
+
+        match node.data.node_type {
+            NodeType::File => {
+                // println!("{} -> {}", node.data.original_path.display(), node.data.path.display());
+                create_all_parents(&abs_path);
+                let _ = fs::rename(&abs_original_path, &abs_path);
+            }
+            _ => (),
+        }
+    }
+
+    for node in from_tree.nodes.iter() {
+        let mut abs_path = to_path.clone();
+        abs_path.push(&node.data.path);
+        let mut abs_original_path = from_path.clone();
+        abs_original_path.push(&node.data.original_path);
+
+        println!("{} {}", node.data.path.display(), abs_path.display());
+
+        match node.data.node_type {
+            NodeType::Directory => match to_tree.path_map.get(&node.data.path) {
+                None => {
+                    let _ = fs::remove_dir_all(&abs_path);
+                    let _ = fs::remove_dir(&abs_path);
+                }
+                Some(_) => (),
+            },
+            _ => (),
+        }
+    }
+}
+
+// validation-loading ideas
+// the naive approach is to just full load the tree and then validate
+// if the tree is massive and obviously fucked early this is dumb
+// better idea?
+// validate current tree every k nodes, doubling k each time
+
+// need to edit to take a closure (tree) -> bool
+
+// pub fn load_tree_val(root: FileIden) -> FileSysTree {
+//     let mut tree: FileSysTree = Tree::new();
+
+//     return tree;
+// }
+
+// super simple approach, a better one would use a struct with a validation trait that statefully tracks what has been validified, and pass difference from last time
+// but this would be sophisticated enough for now
---
+++ caylang/test/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod filesys;
+pub mod filesys_builder;
+pub mod test_helpers;
