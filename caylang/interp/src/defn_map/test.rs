ee00c2190af0905fcc49cddd5ed6d14efc7ded6f
Kris Parekh <krispa1919@gmail.com>
2023-10-23 13:07:22 +1100

    Added some UTs for params

--- caylang/interp/src/defn_map/test.rs
+++ caylang/interp/src/defn_map/test.rs
@@ -1,6 +1,6 @@
 use caylang_parser::ast::{SuperIdent, Ident, NodePrototype, Prototype, NodeType};
 use crate::defn_map::{new_defn_map, DefnMap};
-use caylang_parser::test_helpers::{to_ident};
+use caylang_parser::test_helpers::{to_ident, to_str_param_ident};
 
 #[test]
 fn test_new() {
@@ -17,6 +17,19 @@ fn test_default_dir() {
     match dir {
         Ok(Prototype::NodePrototype(p)) => {
             assert!(p.regex == r".*".to_string());
+            assert!(matches!(p.node_type, NodeType::Dir));
+        }
+        _ => unreachable!()
+    }
+}
+
+fn test_param_dir() {
+    let mut d = new_defn_map();    
+    let dir = d.get_object(&SuperIdent::ParamIdent(to_str_param_ident("Directory", "r\"test\"")));
+    assert!(matches!(&dir, Ok(Prototype::NodePrototype(p))));
+    match dir {
+        Ok(Prototype::NodePrototype(p)) => {
+            assert!(p.regex == r"test".to_string());
             assert!(matches!(p.node_type, NodeType::Dir));
         }
         _ => unreachable!()
